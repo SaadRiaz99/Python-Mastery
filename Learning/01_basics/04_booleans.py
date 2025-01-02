a281600d23e5c695d4e6ad2a18222a9bb44b999f
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-01-02 20:40:00 +0500

    Learn: Boolean operations

---
+++ Learning/01_basics/04_booleans.py
@@ -0,0 +1,9 @@
+﻿# Booleans
+x = True
+y = False
+print(f'And: {x and y}')
+print(f'Or: {x or y}')
+print(f'Not: {not x}')
+print(f'Bool of 0: {bool(0)}')
+print(f'Bool of 1: {bool(1)}')
+print(f'Bool of string: {bool(\
