257967d7819048ded90a0c412b29a093d21b9e16
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-01-04 00:23:00 +0500

    Learn: None type

---
+++ Learning/01_basics/05_none.py
@@ -0,0 +1,14 @@
+﻿# None Type
+result = None
+print(f'Type: {type(result)}')
+print(f'Is None: {result is None}')
+print(f'Is not None: {result is not None}')
+
+def find_user(id):
+    if id == 1:
+        return {'name': 'Saad'}
+    return None
+
+user = find_user(1)
+if user is not None:
+    print(f'Found: {user[\
