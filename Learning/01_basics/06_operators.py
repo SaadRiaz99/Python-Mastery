50007cc5a55cb8d5d8d517f803927f1d589227dc
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-01-05 22:34:00 +0500

    Learn: All operators in Python

---
+++ Learning/01_basics/06_operators.py
@@ -0,0 +1,33 @@
+﻿# Operators
+# Arithmetic
+print(f'Add: {5 + 3}')
+print(f'Sub: {5 - 3}')
+print(f'Mul: {5 * 3}')
+print(f'Div: {5 / 3}')
+print(f'Floor: {5 // 3}')
+print(f'Mod: {5 % 3}')
+print(f'Power: {5 ** 3}')
+
+# Comparison
+print(f'Equal: {5 == 5}')
+print(f'Not equal: {5 != 3}')
+print(f'Greater: {5 > 3}')
+print(f'Less: {5 < 3}')
+
+# Assignment
+x = 10
+x += 5
+print(f'x after +=: {x}')
+
+# Identity
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+print(f'a is b: {a is b}')
+print(f'a is c: {a is c}')
+print(f'a == c: {a == c}')
+
+# Membership
+print(f'2 in [1,2,3]: {2 in [1,2,3]}')
+print(f'4 in [1,2,3]: {4 in [1,2,3]}')
+
