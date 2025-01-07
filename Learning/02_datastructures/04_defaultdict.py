4f0c09c17fea9bfca195b52013c8d30a67a27134
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-01-07 19:39:00 +0500

    Learn: defaultdict usage

---
+++ Learning/02_datastructures/04_defaultdict.py
@@ -0,0 +1,24 @@
+﻿# defaultdict
+from collections import defaultdict
+
+# Group by first letter
+words = ['apple', 'banana', 'avocado', 'blueberry', 'cherry']
+d = defaultdict(list)
+for word in words:
+    d[word[0]].append(word)
+print(dict(d))
+
+# Count with defaultdict
+text = 'hello world hello python'
+counter = defaultdict(int)
+for word in text.split():
+    counter[word] += 1
+print(dict(counter))
+
+# Set defaultdict
+students = defaultdict(set)
+students['math'].add('Alice')
+students['math'].add('Bob')
+students['science'].add('Alice')
+print(dict(students))
+
