665bd802e2d12d6af0b66dd74175183ed9828b51
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-01-14 20:33:00 +0500

    Learn: classmethod and staticmethod

---
+++ Learning/04_oop/05_classmethod.py
@@ -0,0 +1,26 @@
+﻿# classmethod and staticmethod
+
+class Date:
+    def __init__(self, day, month, year):
+        self.day = day
+        self.month = month
+        self.year = year
+
+    @classmethod
+    def from_string(cls, date_str):
+        d, m, y = map(int, date_str.split('-'))
+        return cls(d, m, y)
+
+    @staticmethod
+    def is_valid(d, m, y):
+        return 1 <= m <= 12 and 1 <= d <= 31
+
+    def __str__(self):
+        return f'{self.day}/{self.month}/{self.year}'
+
+d1 = Date(15, 1, 2025)
+d2 = Date.from_string('20-3-2025')
+print(f'Date 1: {d1}')
+print(f'Date 2: {d2}')
+print(f'Valid: {Date.is_valid(15, 13, 2025)}')
+
