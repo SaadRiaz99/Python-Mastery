fc0550b145792ebfdf0cfac0feb0f4d08e1d4043
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-01-24 02:56:00 +0500

    Learn: Database context manager

---
+++ Learning/09_context_managers/02_database.py
@@ -0,0 +1,35 @@
+﻿# Database Context Manager
+
+from contextlib import contextmanager
+
+class MockDB:
+    def __init__(self):
+        self.connected = False
+        self.data = {}
+
+    def connect(self):
+        self.connected = True
+        print('Connected to database')
+
+    def disconnect(self):
+        self.connected = False
+        print('Disconnected from database')
+
+    def execute(self, query):
+        if not self.connected:
+            raise RuntimeError('Not connected')
+        print(f'Executing: {query}')
+        return []
+
+@contextmanager
+def database():
+    db = MockDB()
+    db.connect()
+    try:
+        yield db
+    finally:
+        db.disconnect()
+
+with database() as db:
+    db.execute('SELECT * FROM users')
+    db.execute('INSERT INTO users VALUES (1, \
