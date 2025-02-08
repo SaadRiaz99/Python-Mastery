bd18aabdc61cdc894404698b3475dc7bc34a3899
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-02-09 02:23:00 +0500

    Learn: Advanced collections patterns

---
+++ Learning/18_collections/02_advanced.py
@@ -0,0 +1,10 @@
+﻿# Advanced Collections
+
+from collections import ChainMap, UserDict, UserList
+
+# ChainMap - combine dicts
+defaults = {'color': 'red', 'user': 'guest'}
+environment = {'user': 'admin'}
+config = ChainMap(environment, defaults)
+
+print(f'User: {config[\
