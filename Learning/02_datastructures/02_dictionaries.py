9de6945e0538e8fb7ed36e88e5b84d8bbe1a86a7
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-05-19 21:27:00 +0500

    Learn: Python dictionaries

---
+++ Learning/02_datastructures/02_dictionaries.py
@@ -0,0 +1,13 @@
+﻿# Python Dictionaries
+
+# Creating dictionaries
+person = {
+    'name': 'Saad',
+    'age': 25,
+    'city': 'Karachi',
+    'is_student': True
+}
+print(f'Person: {person}')
+
+# Accessing values
+print(f'Name: {person[\
