2d9464394ffe56e5a40658429fb31c1e8cf08e63
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-09-02 01:58:00 +0500

    Learn: Metaclasses

---
+++ Learning/24_advanced/01_metaclasses.py
@@ -0,0 +1,12 @@
+﻿# Metaclasses
+
+class Meta(type):
+    def __new__(cls, name, bases, dict):
+        dict['class_id'] = id(cls)
+        return super().__new__(cls, name, bases, dict)
+
+class MyClass(metaclass=Meta):
+    pass
+
+print(f'Class ID: {MyClass.class_id}')
+
