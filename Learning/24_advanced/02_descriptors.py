cfed42d5939579d1e85a955bd36001417d5cf2a0
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-09-03 00:42:00 +0500

    Learn: Descriptors

---
+++ Learning/24_advanced/02_descriptors.py
@@ -0,0 +1,23 @@
+﻿# Descriptors
+
+class Validator:
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        return obj.__dict__.get(self.name)
+
+    def __set__(self, obj, value):
+        if not isinstance(value, int):
+            raise TypeError(f'{self.name} must be int')
+        obj.__dict__[self.name] = value
+
+class Person:
+    age = Validator()
+
+p = Person()
+p.age = 25
+print(f'Age: {p.age}')
+
