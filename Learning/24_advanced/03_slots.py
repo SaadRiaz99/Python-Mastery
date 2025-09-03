e29425387fd694ad59622485e6a2410ae0f9068f
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-09-03 21:22:00 +0500

    Learn: __slots__ for memory optimization

---
+++ Learning/24_advanced/03_slots.py
@@ -0,0 +1,21 @@
+﻿# __slots__
+
+class PointSlots:
+    __slots__ = ('x', 'y')
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class PointNoSlots:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+import sys
+p1 = PointSlots(1, 2)
+p2 = PointNoSlots(1, 2)
+
+print(f'With slots: {sys.getsizeof(p1)} bytes')
+print(f'Without slots: {sys.getsizeof(p2)} bytes')
+print(f'Has dict: {hasattr(p1, \
