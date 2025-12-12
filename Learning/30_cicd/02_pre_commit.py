12c1fc46e254f3a350f08d74eb8c4c51d24b604e
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-12-12 18:30:00 +0500

    Learn: Pre-commit hooks patterns

---
+++ Learning/30_cicd/02_pre_commit.py
@@ -0,0 +1,18 @@
+﻿PRE_COMMIT = '''
+repos:
+- repo: https://github.com/pre-commit/pre-commit-hooks
+  rev: v4.4.0
+  hooks:
+  - id: trailing-whitespace
+  - id: end-of-file-fixer
+  - id: check-yaml
+- repo: https://github.com/psf/black
+  rev: 23.3.0
+  hooks:
+  - id: black
+'''
+
+PYPROJECT = '''
+[tool.black]
+line-length = 88
+target-version = [\
