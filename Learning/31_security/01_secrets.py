edee75aabe5afd1675980e5fb3878e12da1e70f9
Saad Bin Riaz <saadriaz99@users.noreply.github.com>
2025-12-13 23:53:00 +0500

    Learn: Managing secrets

---
+++ Learning/31_security/01_secrets.py
@@ -0,0 +1,18 @@
+﻿import os
+
+def get_secret(key, default=None):
+    return os.getenv(key, default)
+
+class SecretManager:
+    def __init__(self):
+        self._secrets = {}
+
+    def set(self, key, value):
+        self._secrets[key] = value
+
+    def get(self, key):
+        return self._secrets.get(key)
+
+manager = SecretManager()
+manager.set('api_key', 'secret123')
+print(f'API Key: {manager.get(\
