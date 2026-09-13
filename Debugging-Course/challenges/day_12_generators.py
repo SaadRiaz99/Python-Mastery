cd5239d4a5d29214df20a40ad2d9d448ce829b5b
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-13 12:49:11 +0500

    Add type hint to count_to function and fix return statement to yield values

--- Debugging-Course/challenges/day_12_generators.py
+++ Debugging-Course/challenges/day_12_generators.py
@@ -1,9 +1,9 @@
 """Day 12: Generate 1, 2, 3 without returning early."""
 
-def count_to(limit):
+def count_to(limit : int):
     number = 1
     while number <= limit:
-        return number
+        yield number
         number += 1
 
 assert list(count_to(3)) == [1, 2, 3]
