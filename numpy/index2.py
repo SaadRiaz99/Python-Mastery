84fea06ed3e959367331b19d5d31d6cec73747a3
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-16 13:18:43 +0500

    feat: add name list and array representation to temperature script

--- numpy/index2.py
+++ numpy/index2.py
@@ -4,3 +4,12 @@ temperature = [50.2, 72.5, 51.7, 41.0, 36.5]
 
 # np.mean(temperature)
 print("The average temperature is:", np.mean(temperature))
+
+
+name = ["Saad Riaz", "Talha Riaz", "Jane Smith", "Umar Riaz", "Sumaiya Rani"]
+
+print(name)
+
+name_array = np.array(["Saad Riaz", "Talha Riaz", "Jane Smith", "Umar Riaz", "Sumaiya Rani"])
+
+print(name_array)
