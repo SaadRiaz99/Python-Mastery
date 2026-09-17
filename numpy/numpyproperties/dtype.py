0dce76a64387e5fab4454b635bf8a7bfdde3b9aa
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-17 11:12:26 +0500

    feat: add dtype.py to demonstrate data types of numpy arrays

---
+++ numpy/numpyproperties/dtype.py
@@ -0,0 +1,6 @@
+import numpy as np 
+temperature = np.array([50.2, 72.5, 51.7, 41.0, 36.5])
+name = np.array(["Saad Riaz", "Talha Riaz", "Jane Smith", "Umar Riaz", "Sumaiya Rani"])
+
+print("Data type of the temperature array:", temperature.dtype)
+print("Data type of the name array:", name.dtype)
