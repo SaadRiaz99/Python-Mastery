2ea05fe2ee13fe2ab64e6453aa1110bec7ce600a
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-17 11:29:17 +0500

    feat: add math operator demonstrations using NumPy

---
+++ numpy/numpyproperties/mathoperator.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a = np.array([17, 25, 30])
+print("Original array:", a)
+print("Data type of the array:", a.dtype)
+print("Size of the array:", a.size)
+print("Shape of the array:", a.shape)
+#Math operators
+print("Sum of the array elements:", np.sum(a))
+print("Mean of the array elements:", np.mean(a))
+print(a + 5)
+print(a - 5)
+print(a * 5)
+print(a / 5)
