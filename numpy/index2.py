19c0e41f683eeb5466da45ca3568f999cc86bde7
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-17 10:28:21 +0500

    feat: add multi-dimensional array examples in index3.py and clean up index2.py

--- numpy/index2.py
+++ numpy/index2.py
@@ -4,12 +4,14 @@ temperature = [50.2, 72.5, 51.7, 41.0, 36.5]
 
 # np.mean(temperature)
 print("The average temperature is:", np.mean(temperature))
-
-
+#
+##list
 name = ["Saad Riaz", "Talha Riaz", "Jane Smith", "Umar Riaz", "Sumaiya Rani"]
 
 print(name)
-
+##array
 name_array = np.array(["Saad Riaz", "Talha Riaz", "Jane Smith", "Umar Riaz", "Sumaiya Rani"])
 
 print(name_array)
+#1 dimension
+array_1d = np.array(
---
+++ numpy/index3.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+#1 dimension array
+
+d1_array = np.array([1, 2, 3, 4, 5])
+
+#2 dimension array
+d2_array = np.array([[1, 2, 3], [4, 5, 6]])
+
+#3 dimension array
+d3_array = np.array([[[1, 2],
+                    [3, 4]], 
+                    [[5, 6], 
+                    [7, 8]]])
+
+print("1D Array:")
+print(d1_array) 
+print("\n2D Array:")
+print(d2_array)
+print("\n3D Array:")
+print(d3_array) 
