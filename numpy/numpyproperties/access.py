366e238c1a30cf7a9ad8c03358df768d85063590
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-19 16:19:35 +0500

    feat: update array slicing example in access.py for clarity

--- numpy/numpyproperties/access.py
+++ numpy/numpyproperties/access.py
@@ -3,6 +3,6 @@ import numpy as np
 name = np.array(["Saad Riaz", "Talha Riaz", "Jane Smith", "Umar Riaz", "Sumaiya Rani"])
 print(name[-1])
 #[start:stop:step]
-print(name[3:5:1])
+print(name[0:2:4])
 print(name[:1])
 print(name[::3])
