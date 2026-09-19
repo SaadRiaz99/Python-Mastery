102f3633c6f177cad5b9859568825f7ede6c089b
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-19 16:16:51 +0500

    feat: update access.py to demonstrate advanced NumPy array slicing

--- numpy/numpyproperties/access.py
+++ numpy/numpyproperties/access.py
@@ -2,4 +2,7 @@ import numpy as np
 
 name = np.array(["Saad Riaz", "Talha Riaz", "Jane Smith", "Umar Riaz", "Sumaiya Rani"])
 print(name[-1])
-print(name[0])
+#[start:stop:step]
+print(name[3:5:1])
+print(name[:1])
+print(name[::3])
