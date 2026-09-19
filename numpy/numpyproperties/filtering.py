fix: update boolean masking example in filtering.py for clarity

numpy/numpyproperties/filtering.py
@@ -1,4 +1,4 @@
 import numpy as np
-
+#boolean masking
 names = np.array(["Saad Riaz", "Talha Riaz", "Jane Smith", "Umar Riaz", "Sumaiya Rani"])
-print(names[-1])  # Accessing the last element of the array
+print(names[names == "Sumaiya Rani"])  # Accessing the last element of the array