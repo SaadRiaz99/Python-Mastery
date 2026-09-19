378c3cb72f852fdc9e54855b5e95fb58edbeee0a
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-19 16:32:25 +0500

    feat: add temperature array and boolean masking example in filtering.py

--- numpy/numpyproperties/filtering.py
+++ numpy/numpyproperties/filtering.py
@@ -1,4 +1,6 @@
 import numpy as np
 #boolean masking
 names = np.array(["Saad Riaz", "Talha Riaz", "Jane Smith", "Umar Riaz", "Sumaiya Rani"])
+tempreatures = np.array([30, 25, 20, 15, 10])
+print(tempreatures > 10)  # Accessing the last element of the array
 print(names[names == "Sumaiya Rani"])  # Accessing the last element of the array
