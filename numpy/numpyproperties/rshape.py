cfbf0111bdf2035913342b654f9d880112f9d2cf
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-19 16:44:15 +0500

    feat: update temperature array and reshape output in rshape.py

--- numpy/numpyproperties/rshape.py
+++ numpy/numpyproperties/rshape.py
@@ -1,3 +1,5 @@
 import numpy as np
 
-tempreatures = np.array([30, 25, 20, 15, 10])
+tempreatures = np.array([30, 25, 20, 15, 10 ,11 , 12, 13, 14])
+reshap = tempreatures.reshape((3, 3))
+print(reshap)
