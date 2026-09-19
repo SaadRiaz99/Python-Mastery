671f6420c7451e167581654f1348c4dd90cdd622
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-19 16:51:03 +0500

    feat: add num array for reshaping in rshape.py

--- numpy/numpyproperties/rshape.py
+++ numpy/numpyproperties/rshape.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 tempreatures = np.array([30, 25, 20, 15, 10 ,11 , 12, 13, 14])
+num = np.array([15, 30, 25, 20, 15, 10 ,11 , 12, 13, 14])
 reshap = tempreatures.reshape((3, 3))
 print(reshap)
