91171c7a5b00e390a03df9167a76d47bb1cdc2de
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-23 18:15:43 +0500

    Add example for vectorized addition using NumPy

--- numpy/broadcasting/1list.py
+++ numpy/broadcasting/1list.py
@@ -5,3 +5,11 @@ list2 = [4, 5, 6]
 
 result = [x+y for x,y in zip(list1, list2)]
 print("Result of addition: ", result)
+
+# isi ko vectorized approcah karte hain 
+
+lst1 = np.array([1, 2, 3])
+lst2 = np.array([4, 5, 6])
+
+result = lst1 + lst2
+print("Result of addition using numpy: ", result)
