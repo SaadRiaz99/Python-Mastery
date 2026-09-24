b77d02b16362b6b68898dc43005c610535fcef16
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-24 18:24:55 +0500

    Refactor NaN handling in array example and update output

--- numpy/handling/handof.py
+++ numpy/handling/handof.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5 , np.nan , np.nan])
-
+num_cler = np.nan_to_num(arr, nan=12)
 na = np.isnan(arr)
 print("Is NaN: ", na)
-print(np.nan == np.nan)
+print(num_cler)
+ 
