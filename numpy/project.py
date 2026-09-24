e6fc97108d8194027f0e19ff7b71043db508afbf
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-24 23:38:30 +0500

    Remove duplicate sales data array and retain print statement for sales data output

--- numpy/project.py
+++ numpy/project.py
@@ -17,9 +17,4 @@ sales = np.array([
     [3, 2, 4, 1, 5, 6, 3],       # Keyboard
     [4, 3, 2, 5, 6, 8, 4]        # Headphones
 ])
-sales = np.array([
-    [2, 1, 3, 0, 2, 4, 1],       # Laptop
-    [5, 8, 6, 4, 10, 12, 7],     # Mouse
-    [3, 2, 4, 1, 5, 6, 3],       # Keyboard
-    [4, 3, 2, 5, 6, 8, 4]        # Headphones
-])
+print("Sales Data:", sales)
