663263d4e6d9a0360794f0f84f257e0a645e97d8
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-25 12:23:39 +0500

    Add initial save.py script and Filesave.csv for student data

---
+++ pandas/save.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = {
+   "Name": ["Saad Bin Riaz" , "Sumaiya Rani" , "Talha Riaz"],
+   "Age" : [19 ,25 ,9] ,
+   "City" : ["Jhang" , "Jhang" ,"Jhang"]
+}
+sv = pd.DataFrame(data)
+print(sv)
+
+
+sv.to_csv("Pandas/Filesave.csv" ,index =False )
