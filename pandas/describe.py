82abe1607fa475be1aa1c3dbc187d528f46a5108
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-09-26 12:21:19 +0500

    Add data structure to describe.py with sample data and DataFrame display

--- pandas/describe.py
+++ pandas/describe.py
@@ -2,5 +2,12 @@ import pandas as pd
 data = {
    "Name": ["Saad Bin Riaz" , "Sumaiya Rani" , "Talha Riaz"],
    "Age" : [19 ,25 ,9] ,
-   "City" : ["Jhang" , "Jhang" ,"Jhang"]
+   "City" : ["Jhang" , "Jhang" ,"Jhang"],
+   "Salary":[1000 , 5000 ,6000],
+   "performance":[10,90,58]
+
 }
+frm = pd.DataFrame(data)
+print("Save Data Frame")
+print(frm)
+
